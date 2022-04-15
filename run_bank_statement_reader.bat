9049a25813d72475437fdcca3eda19bf988a7f1e
Azzubair Azeman <azzubair01@student.usm.my>
2022-04-15 22:42:43 +0800

    updated bat file

---
+++ run_bank_statement_reader.bat
@@ -0,0 +1,8 @@
+call conda activate Bank_Statement_Digitization
+cd C:\Users\DataMicron\Desktop\DataMicron\RND\Github\Bank_Statement_Digitization\
+python "13. test.py"
+python "14. pdf_extract_table.py"
+python "15. extract_metadata.py"
+
+
+
