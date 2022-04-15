722353e3e40f82be8d22a1677ab8f361153fe99e
Azzubair Azeman <azzubair01@student.usm.my>
2022-04-15 22:23:59 +0800

    fixed bugs and organised paths

--- 15. extract_metadata.py
+++ 15. extract_metadata.py
@@ -11,28 +11,21 @@ from pytesseract import Output
 
 # --------------------------------------------------------------------------------------------------------------
 
-# ## Prediction on CIMB transactions
-# coordinates_df = pd.read_csv('test_cimb\\saved_model\\saved_model\\predicted_coordinates.csv')
-# # input_path = 'test_cimb\\saved_model\\saved_model\\test\\'
-# input_path = 'test_cimb\\saved_model\\saved_model\\test_new\\'
-# process_path = 'test_cimb\\saved_model\\saved_model\\processed_images\\'
-# output_path = 'test_cimb\\saved_model\\saved_model\\transactions\\'
-# excel_files = sorted(os.listdir('test_cimb\\saved_model\\saved_model\\transactions\\'))
-
-# --------------------------------------------------------------------------------------------------------------
-
-# Prediction on Maybank transactions
-coordinates_df = pd.read_csv('test_mayb\\saved_model\\saved_model\\predicted_coordinates.csv')
-# input_path = 'test_mayb\\saved_model\\saved_model\\test\\'
-input_path = 'test_mayb\\saved_model\\saved_model\\test_new\\'
-process_path = 'test_mayb\\saved_model\\saved_model\\processed_images\\'
-output_path = 'test_mayb\\saved_model\\saved_model\\transactions\\'
-excel_files = sorted(os.listdir('test_mayb\\saved_model\\saved_model\\transactions\\'))
+## Prediction on CIMB transactions
+coordinates_df = pd.read_csv('C:\\Users\\DataMicron\\Desktop\\Bank_Statement_Reader\\predicted_coordinates_cimb.csv')
+input_path = 'C:\\Users\\DataMicron\\Desktop\\Bank_Statement_Reader\\processing_images\\cimb\\'
+process_path = 'C:\\Users\\DataMicron\\Desktop\\Bank_Statement_Reader\\cropped_images\\cimb\\'
+output_path = 'C:\\Users\\DataMicron\\Desktop\\Bank_Statement_Reader\\overall_output\\'
+excel_file_path = 'C:\\Users\\DataMicron\\Desktop\\Bank_Statement_Reader\\transaction_output\\cimb\\'
+excel_files = sorted(os.listdir(excel_file_path))
 
 # --------------------------------------------------------------------------------------------------------------
 
 if not os.path.exists(process_path):
     subprocess.call("powershell mkdir " + process_path)
+
+if not os.path.exists(output_path):
+    subprocess.call("powershell mkdir " + output_path)
 
 images = glob.glob1(input_path, "*.jpg")
 tessdata_dir_config = r'-l "eng+msa" --tessdata-dir "C:\Program Files\Tesseract-OCR\tessdata" --psm 6'
@@ -75,16 +68,92 @@ raw_info = pd.DataFrame(rows, columns=['Image', 'Key', 'Value'])
 # --------------------------------------------------------------------------------------------------------------
 
 for i, excel in tqdm(enumerate(excel_files)):
-    output_df = pd.read_excel(output_path + excel)
+    output_df = pd.read_excel(excel_file_path + excel)
+    statement_name1 = excel[12:-5]
     with pd.ExcelWriter(output_path + excel[:-5] + '.xlsx', engine='xlsxwriter') as writer:
         rows = []
         for j in range(len(raw_info)):
-            if (excel[12:-5] == raw_info['Image'][j][:15]) and (raw_info['Key'][j] != 'transactions'):
+            statement_name2 = raw_info['Image'][j][:15]
+            if (statement_name1 == statement_name2) and (raw_info['Key'][j] != 'transactions'):
                 row = list(raw_info.iloc[j][1:])
                 rows.append(row)
-                df = pd.DataFrame(rows, columns = ['Key', 'Value'])
+                df = pd.DataFrame(rows, columns=['Key', 'Value'])
                 df = df.drop_duplicates()
         output_df.to_excel(writer, sheet_name='transactions', index=False)
         df.to_excel(writer, sheet_name='metadata', index=False)
+
+
+# --------------------------------------------------------------------------------------------------------------
+
+# # Prediction on Maybank transactions
+coordinates_df2 = pd.read_csv('C:\\Users\\DataMicron\\Desktop\\Bank_Statement_Reader\\predicted_coordinates_mayb.csv')
+input_path2 = 'C:\\Users\\DataMicron\\Desktop\\Bank_Statement_Reader\\processing_images\\mayb\\'
+process_path2 = 'C:\\Users\\DataMicron\\Desktop\\Bank_Statement_Reader\\cropped_images\\mayb\\'
+output_path2 = 'C:\\Users\\DataMicron\\Desktop\\Bank_Statement_Reader\\overall_output\\'
+excel_file_path2 = 'C:\\Users\\DataMicron\\Desktop\\Bank_Statement_Reader\\transaction_output\\mayb\\'
+excel_files2 = sorted(os.listdir(excel_file_path2))
+
+# --------------------------------------------------------------------------------------------------------------
+
+if not os.path.exists(process_path2):
+    subprocess.call("powershell mkdir " + process_path2)
+
+if not os.path.exists(output_path2):
+    subprocess.call("powershell mkdir " + output_path2)
+
+images2 = glob.glob1(input_path2, "*.jpg")
+tessdata_dir_config = r'-l "eng+msa" --tessdata-dir "C:\Program Files\Tesseract-OCR\tessdata" --psm 6'
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+# --------------------------------------------------------------------------------------------------------------
+
+rows2 = []
+for i in tqdm(range(len(coordinates_df2))):
+    for j in range(len(images2)):
+        if coordinates_df2['Image'][i] == images2[j]:
+            image = Image.open(input_path2 + images2[j])
+            results = pytesseract.image_to_data(image, config=tessdata_dir_config, output_type=Output.DICT)
+
+            # (This is not mandatory)
+            width, height = image.size
+
+            # Setting the points for cropped image
+            left = coordinates_df2['xmin'][i] * width
+            top = coordinates_df2['ymin'][i] * height
+            right = coordinates_df2['xmax'][i] * width
+            bottom = coordinates_df2['ymax'][i] * height
+
+            # Cropped image of above dimension
+            # (It will not change orginal image)
+            im1 = image.crop((left, top, right, bottom))
+            im1 = im1.resize((im1.size[0] * 5, im1.size[1] * 5), resample = 5)
+
+            # # Shows the image in image viewer
+            # im1.show()
+            im1.save(process_path2 + images2[j])
+            text = pytesseract.image_to_string(im1, lang='eng+msa', config=tessdata_dir_config)
+
+            row = list([images2[j], coordinates_df2['Classes'][i], text])
+            rows2.append(row)
+
+
+raw_info2 = pd.DataFrame(rows2, columns=['Image', 'Key', 'Value'])
+
+# --------------------------------------------------------------------------------------------------------------
+
+for i, excel in tqdm(enumerate(excel_files2)):
+    output_df2 = pd.read_excel(excel_file_path2 + excel)
+    statement_name1 = excel[12:-5]
+    with pd.ExcelWriter(output_path2 + excel[:-5] + '.xlsx', engine='xlsxwriter') as writer2:
+        rows = []
+        for j in range(len(raw_info2)):
+            statement_name2 = raw_info2['Image'][j][:18]
+            if (statement_name1 == statement_name2) and (raw_info2['Key'][j] != 'transactions'):
+                row = list(raw_info2.iloc[j][1:])
+                rows.append(row)
+                df2 = pd.DataFrame(rows, columns=['Key', 'Value'])
+                df2 = df2.drop_duplicates()
+        output_df2.to_excel(writer2, sheet_name='transactions', index=False)
+        df2.to_excel(writer2, sheet_name='metadata', index=False)
 
 # --------------------------------------------------------------------------------------------------------------
